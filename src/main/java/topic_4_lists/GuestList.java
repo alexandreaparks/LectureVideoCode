05526dddc3970f0ff8822813afca4d89d86c891b
alexandrea parks <alparks@idk.com>
2023-10-18 12:05:55 -0500

    Finished Topic 4 Lecture Video Code

--- src/main/java/topic_4_lists/GuestList.java
+++ src/main/java/topic_4_lists/GuestList.java
@@ -9,7 +9,6 @@ import java.util.Collections;
 public class GuestList {
     public static void main(String[] args) {
 
-        // TODO ENDED GUEST LIST VIDEO AT 10:16
 
         // Random rnd = new Random();
         // int randomNumber = rnd.nextInt(10); // random number 0-9
@@ -34,13 +33,65 @@ public class GuestList {
         Collections.sort(guestList);
 
         printGuestNames(guestList);
+
+        // remove guests from list if needed
+
+        while (yesNoInput("Do you want to remove any guests?")) {
+            System.out.println("Here are all the guests: ");
+            printGuestNames(guestList);
+
+            if (guestList.isEmpty()) {
+                System.out.println("There are no more guests to remove");
+                break;
+            }
+
+            int index = positiveIntInput("Enter the number of the guest you want to remove: ");
+            index--;
+            if (index < guestList.size()) {
+                String removedName = guestList.remove(index);
+                System.out.println("Removed guest " + removedName);
+            } else {
+                System.out.println("Error - please enter a valid number of a guest");
+            }
+
+        }
+
+        printGuestNames(guestList);
+        System.out.println("The number of guests on the guest list is " + guestList.size());
+        System.out.println();
+        
+        selectPrizeWinner(guestList);
+
+
+
+
     }
+
+    private static void selectPrizeWinner(List<String> guests) {
+
+        if (guests.isEmpty()) {
+            System.out.println("Warning - no guests - no one to win the prize");
+            return;
+        }
+        Random rng = new Random();
+        int randomGuestIndex = rng.nextInt(guests.size());
+
+        String prizeWinner = guests.get(randomGuestIndex);
+        System.out.println("Guest number " + (randomGuestIndex+1) + " " + prizeWinner + " wins a prize!");
+    }
+
 
     public static void printGuestNames(List<String> guests) {
 
         System.out.println("Your guest list is: \n");
-        for (int i = 0; i < guests.size(); i++) {
-            System.out.println((i + 1) + ": " + guests.get(i));
+
+        if (guests.isEmpty()) {
+            System.out.println("There are no guests in the list.");
+        } else {
+            for (int i = 0; i < guests.size(); i++) {
+                System.out.println((i + 1) + ": " + guests.get(i));
+            }
+            System.out.println();
         }
     }
 
@@ -52,4 +103,6 @@ public class GuestList {
         }
         return false;
     }
+
+
 }
