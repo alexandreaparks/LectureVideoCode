94d162c3a814ac310c61d8dc29acba3c79bce346
alexandrea parks <alparks@idk.com>
2023-11-21 20:00:02 -0600

    Finished Topic 6 Lecture Video Code

---
+++ src/main/java/topic_6_files_exceptions/UserEntersDataDo.java
@@ -0,0 +1,17 @@
+package topic_6_files_exceptions;
+
+import static input.InputUtils.intInput;
+
+public class UserEntersDataDo {
+    public static void main(String[] args) {
+
+        // Example - you want the user to enter a number between 1 and 10
+
+        int userInput;
+        do { // do this while the condition is true
+            userInput = intInput("Please enter a number between 1 and 10: ");
+        } while (userInput < 1 || userInput > 10);
+
+        System.out.println("Thanks, the number you entered is " + userInput);
+    }
+}
